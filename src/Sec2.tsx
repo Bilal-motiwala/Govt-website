41ff4ee8a7ba4ca5cd5dec9f125412b1ef408acc
Bilal-motiwala <bilalmotiwala20@gmail.com>
2024-11-26 12:39:26 +0500

    added md:

--- src/Sec2.tsx
+++ src/Sec2.tsx
@@ -3,15 +3,15 @@ const Sec2 = () => {
     <>
     <main className="flex justify-around md:flex-row">
 
-    <div className="w-full w-3/12 border-blue-600">
+    <div className="w-full md:w-3/12 border-blue-600">
     <image src="/pic1.jpg" width={400} height={400} alt="pic 1" />
     </div>
 
-    <div className="w-full w-3/12 border-blue-600">
+    <div className="w-full md:w-3/12 border-blue-600">
     <image src="/pic2.jpg" width={400} height={400} alt="pic 2" />
     </div>
 
-    <div className="w-full w-3/12 border-blue-600">
+    <div className="w-full md:w-3/12 border-blue-600">
     <image src="/pic3.jpg" width={400} height={400} alt="pic 3" />
     </div>
 
